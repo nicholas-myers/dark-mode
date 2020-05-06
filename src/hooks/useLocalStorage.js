6969117758bc562d928295cc69e1cf72c950674c
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-05-06 12:35:03 -0700

    building useLocalStarge function

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+//key will be a string
+//value can be anything, object, array, string, number, boolean, etc
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (window.localStorage.getItem(key)) {
+      return JSON.parse(window.localStorage.getItem(key));
+    }
+    window.localStorage.setItem(key, JSON.stringify(initialValue));
+    return initialValue;
+  });
+
+  const setValue = value => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue, setValue];
+};
